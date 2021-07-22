b49a73599b299fd45d12c441f5e31782126b7349
jhonpresley <jhon.presley1993@gmail.com>
2021-07-21 22:27:30 -0300

    Adicionando classes: Phone, PhoneType

---
+++ src/main/java/one/digitalinovation/personAPI/enums/PhoneType.java
@@ -0,0 +1,14 @@
+package one.digitalinovation.personAPI.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME(),
+    MOBILE(),
+    COMMERCIAL();
+
+}
